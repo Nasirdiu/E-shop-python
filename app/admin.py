05f981e9f246bde963146917636305e4b83e98d7
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-21 11:27:03 +0600

    product added

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category,Sub_Category
+from .models import Category,Sub_Category,Product
 admin.site.register(Category)
 admin.site.register(Sub_Category)
+admin.site.register(Product)
--- app/models.py
+++ app/models.py
@@ -10,3 +10,12 @@ class Sub_Category(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+class Product(models.Model):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=False,default='')
+    sub_category=models.ForeignKey(Sub_Category,on_delete=models.CASCADE,null=False,default='')
+    image=models.ImageField(upload_to='ecommerce/pimg')
+    name=models.CharField(max_length=180)
+    price=models.IntegerField()
+    date=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.name
