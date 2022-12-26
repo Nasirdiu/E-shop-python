270974f195be707309ba5cd08401c19a64ada330
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-26 23:29:02 +0600

    product features

--- E_shop/urls.py
+++ E_shop/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .import views
 from django.contrib import admin
 from django.urls import path
-from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('master/',views.Master,name='master'),
-    path('',views.Index,name='index')
+    path('',views.Index,name='index'),
+
 ]
+# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
