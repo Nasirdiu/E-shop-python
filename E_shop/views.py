7432fa15d8ffe39ab2c879694d5a7f40e6abad6a
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-26 23:04:12 +0600

    product features

--- E_shop/views.py
+++ E_shop/views.py
@@ -1,12 +1,15 @@
 from  django.shortcuts import render
-from app.models import Category
+from app.models import Category,Product
+
 
 def Master(request):
     return render(request,'master.html')
 
 def Index(request):
     category=Category.objects.all()
+    product=Product.objects.all()
     context={
         'category':category,
+        'product':product,
     }
     return render(request,'index.html',context)
